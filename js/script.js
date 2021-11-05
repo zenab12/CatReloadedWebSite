129bdb71fd4799572e4371e11b63dffc21b59518
zenab mohammed abdelmonaem <78083890+zenab12@users.noreply.github.com>
2021-11-05 23:29:44 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -81,7 +81,7 @@ $(function() {
 
     $(window).on('load', function() {
 
-        $('.loader').find($('img')).fadeOut(3500, function() {
+        $('.loader').find($('img')).fadeOut(1500, function() {
             $('body').css({ "overflow": "auto" });
             $(this).parent().remove();
             $('.mainheader').find($('.overlay')).addClass("animate__backInUp");
